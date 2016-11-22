ebe5f7fa275e2bce8830f7c0abec3cc6a9345fa5
Lukaschub <lukas.schubsda@gmail.com>
2016-11-22 10:02:11 -0800

    commit message

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://yourdomain.com/mail/contact_me.php",
+                url: "https://lukaschub.github.io/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
